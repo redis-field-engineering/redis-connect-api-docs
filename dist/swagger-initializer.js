29385812c9b36e85ea1da224bd5f4e578e9ea353
viragtripathi <virag.tripathi@gmail.com>
2023-07-06 16:26:06 -0400

    replaced swagger yaml with original json

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,20 +1,16 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
-
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
-
-  //</editor-fold>
+window.onload = function () {
+const ui = SwaggerUIBundle({
+  urls: [
+    {
+      name: "job_manager",
+      url: "swagger.json",
+    }
+  ],
+  dom_id: "#swagger-ui",
+  deepLinking: true,
+  presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+  plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+  layout: "StandaloneLayout",
+});
+window.ui = ui;
 };
